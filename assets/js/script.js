f0dc919fdfbacb5412e3e21f8c6d2c8da2e46baf
Daniil Sitdikov <daniil@sitdikov.su>
2017-03-25 21:02:40 +0300

    rm script

---
+++ assets/js/script.js
@@ -0,0 +1,186 @@
+$(document).ready(function () {
+
+    btn_popup_close = '.btn-popup-close';
+
+    btn_contact_animation = $('.btn-contact-animation');
+    btn_case_animation = $('.btn-case-animation');
+    btn_about_animation = $('.btn-about-animation');
+
+    btn_contact = $('.btn-contact');
+    btn_case = $('.btn-case');
+    btn_case_ara = $('.btn-arahipster');
+    btn_case_mg = $('.btn-mg');
+    btn_case_cross = $('.btn-crossless');
+    btn_about = $('.btn-about');
+
+    popup_contact = $('.contact-background');
+    popup_case = $('.popup-case');
+    popup_case_ara = $('.arahipster');
+    popup_case_mg = $('.mg');
+    popup_case_cross = $('.cross');
+    popup_about = $('.about-background');
+
+    function removeFullPageScroll() {
+        $.fn.fullpage.setMouseWheelScrolling(false);
+        $.fn.fullpage.setAllowScrolling(false);
+        $.fn.fullpage.setKeyboardScrolling(false);
+        console.log("Navigation blocked");
+    };
+
+    function allowFullPageScroll() {
+        $.fn.fullpage.setMouseWheelScrolling(true);
+        $.fn.fullpage.setAllowScrolling(true);
+        $.fn.fullpage.setKeyboardScrolling(true);
+        console.log("Navigation allowed");
+    };
+
+    function removeContactClosed() {
+        btn_contact.removeClass('closed');
+        $(btn_popup_close).removeClass('closed');
+        popup_contact.removeClass('closed');
+        btn_contact_animation.removeClass('closed');
+    }
+
+    function removeCaseClosed() {
+        btn_case.removeClass('closed');
+        $(btn_popup_close).removeClass('closed');
+        popup_case.removeClass('closed');
+        btn_case_animation.removeClass('closed');
+    }
+
+    function removeAboutClosed() {
+        btn_about.removeClass('closed');
+        $(btn_popup_close).removeClass('closed');
+        popup_about.removeClass('closed');
+        btn_about_animation.removeClass('closed');
+    }
+
+    $('#fullpage').fullpage({
+        anchors: ['Crossless', 'MindGuests', 'Arahipster'],
+        sectionsColor: ['#C63D0F', '#1BBC9B', '#7E8F7C'],
+        navigation: true,
+        navigationPosition: 'right',
+        navigationTooltips: ['Crossless', 'Mind Guests', 'Arahipster']
+    });
+
+    // Кнопка "Написать нам"
+    btn_contact.on('click', function () {
+        $(this).toggleClass('opened');
+        popup_contact.toggleClass('opened');
+        popup_contact.find(btn_popup_close).toggleClass('opened');
+        removeFullPageScroll();
+    });
+
+    // Кнопка "Открыть кейс Арахипстер"
+    btn_case_ara.on('click', function () {
+        console.log("Case");
+        $(this).toggleClass("opened");
+        popup_case_ara.toggleClass("opened");
+        popup_case_ara.find(btn_popup_close).toggleClass('opened');
+        removeFullPageScroll();
+    });
+
+    // Кнопка "Открыть кейс Mind Guests"
+    btn_case_mg.on('click', function () {
+        console.log("Case");
+        $(this).toggleClass("opened");
+        popup_case_mg.toggleClass("opened");
+        popup_case_mg.find(btn_popup_close).toggleClass('opened');
+        removeFullPageScroll();
+    });
+
+    // Кнопка "Открыть кейс Арахипстер"
+    btn_case_cross.on('click', function () {
+        console.log("Case");
+        $(this).toggleClass("opened");
+        popup_case_cross.toggleClass("opened");
+        popup_case_cross.find(btn_popup_close).toggleClass('opened');
+        removeFullPageScroll();
+    });
+
+    // Надпись "О нас"
+    btn_about.on('click', function () {
+        $(this).toggleClass('opened');
+        popup_about.toggleClass('opened');
+        popup_about.find(btn_popup_close).toggleClass('opened');
+        removeFullPageScroll();
+    });
+
+    // Закрыть кейсы
+    popup_case.find(btn_popup_close).on('click', function () {
+        btn_case.toggleClass('closed');
+        $(this).toggleClass('closed');
+        popup_case.toggleClass('closed');
+        btn_case_animation.toggleClass('closed');
+        setTimeout(removeCaseClosed, 600);
+
+        btn_case.removeClass("opened");
+        popup_case.removeClass("opened");
+        popup_case.find(btn_popup_close).toggleClass('opened');
+
+        allowFullPageScroll();
+    });
+
+    // Закрыть "Написать нам"
+    popup_contact.find(btn_popup_close).on('click', function () {
+        btn_contact.toggleClass('closed');
+        $(this).toggleClass('closed');
+        popup_contact.toggleClass('closed');
+        btn_contact_animation.toggleClass('closed');
+        setTimeout(removeContactClosed, 600);
+
+        btn_contact.removeClass('opened');
+        popup_contact.removeClass('opened');
+        popup_contact.find(btn_popup_close).toggleClass('opened');
+
+        allowFullPageScroll();
+    });
+
+    // Закрыть "О нас"
+    popup_about.find(btn_popup_close).on('click', function () {
+        btn_about.toggleClass('closed');
+        $(this).toggleClass('closed');
+        popup_about.toggleClass('closed');
+        btn_about_animation.toggleClass('closed');
+        setTimeout(removeAboutClosed, 600);
+
+        btn_about.removeClass('opened');
+        popup_about.removeClass('opened');
+        popup_about.find(btn_popup_close).toggleClass('opened');
+
+        allowFullPageScroll();
+    });
+
+    // Текстовое поле в "Написать нам"
+    var textarea = document.querySelector('textarea');
+
+    textarea.addEventListener('keydown', autosize);
+
+    function autosize() {
+        var el = this;
+        setTimeout(function () {
+            el.style.cssText = 'height:auto; padding:0';
+            // for box-sizing other than "content-box" use:
+            // el.style.cssText = '-moz-box-sizing:content-box';
+            el.style.cssText = 'height:' + el.scrollHeight + 'px';
+        }, 0);
+    }
+
+    //Шапка при сролле в кейсах в моб версии
+    popup_case.scroll(function () {
+        var bo = $(this).scrollTop();
+        var a = $(".shapka").css('opacity')
+
+        if (bo >= 200 && a == 0) {
+            $(".shapka").stop().animate({
+                'opacity': '1'
+            }, 500)
+        };
+        if (bo < 200 && a == 1) {
+            $(".shapka").stop().animate({
+                'opacity': '0'
+            }, 500)
+        };
+    })
+
+});
